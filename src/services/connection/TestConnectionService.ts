2a18475fc9ffea413b867f36be4685f9fd72c49d
zaur <zaur.nadiraliev.228@gmail.com>
2020-07-05 01:32:35 +0300

    Function testConnection - little improvements and cleanup

--- src/services/connection/TestConnectionService.ts
+++ src/services/connection/TestConnectionService.ts
@@ -1,24 +1,20 @@
-import Redis, { RedisOptions } from 'ioredis'
+import Redis, { RedisOptions, Redis as TRedis } from 'ioredis'
 
-export function testConnection (options: RedisOptions): Promise<void> {
+export function testConnection (options: RedisOptions): Promise<TRedis> {
   return new Promise((resolve, reject) => {
-    const connection = new Redis(options.port, options.host, {
+    const connection = new Redis({
       enableReadyCheck: true,
       connectTimeout: 3000,
-      password: options.password,
-      retryStrategy () {
-        return null
-      }
+      retryStrategy: () => null,
+      ...options
     })
 
     connection.once('ready', () => {
-      resolve()
-
-      connection.disconnect()
+      resolve(connection)
     })
 
-    connection.once('error', () => {
-      reject(new Error())
+    connection.once('error', error => {
+      reject(error instanceof Error ? error : new Error(error))
     })
   })
 }
