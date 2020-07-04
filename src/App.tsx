da5f440241d1883a838c0cfc1f48cbd3385da684
Leandher <leandher@MacBook-Air-de-Leandher.local>
2020-07-03 21:38:51 -0300

    fix: Theme accessing wrong backgroud attribute

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ document.body.appendChild(modalElement)
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background: ${props => props.theme.colors.background.dark};
+  background: ${props => props.theme.backgrounds.dark};
   overflow: hidden;
   border: 1px solid ${props => props.theme.colors.purpleDark};
   display: flex;
