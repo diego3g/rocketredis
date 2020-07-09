1090124c9a8c9fd8a27f8f07bb198720fef0a2cb
adarshsaraogi <saraogiadarsh@gmail.com>
2020-07-09 20:35:02 +0530

    Removed feather package and updated css style

--- src/components/CodeView/styles.ts
+++ src/components/CodeView/styles.ts
@@ -2,22 +2,31 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   flex: 1;
+  display: flex;
   position: relative;
   height: 100%;
   padding: 16px;
+  justify-content: center;
+  align-items: center;
   background: ${props => props.theme.backgrounds.darker};
-  border-left: 1px solid ${props => props.theme.backgrounds.lightest};
 `
-export const BookIconContainer = styled.span`
-  position: absolute;
-  left: 50%;
-  right: 15.81%;
-  top: 44.11%;
-  bottom: 44.78%;
+
+export const BookIconContainer = styled.div`
+  positon: absolute;
+  text-align: center;
   svg {
     width: 100px;
     height: 100px;
     stroke: #322d41;
     stroke-width: 1px;
+  }
+
+  p {
+    padding-top: 5px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    color: #322d41;
   }
 `
