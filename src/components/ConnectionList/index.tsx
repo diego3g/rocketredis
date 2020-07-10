9da26dc23c26c5e6d425bbb2ac2b7f9f1f56a27c
Diego Fernandes <diego.schell.f@gmail.com>
2020-07-09 22:10:02 -0300

    chore: add connection list styling

--- src/components/ConnectionList/index.tsx
+++ src/components/ConnectionList/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { FiPlusCircle } from 'react-icons/fi'
+import { FiPlusCircle, FiDatabase, FiChevronRight } from 'react-icons/fi'
 import { useToggle } from 'react-use'
 import { useRecoilState } from 'recoil'
 
 import { connectionsState } from '../../atoms/connections'
 import NewConnectionModal from '../NewConnectionModal'
-import { Container, Connections } from './styles'
+import {
+  Container,
+  Connections,
+  Connection,
+  DatabaseList,
+  ConnectionError
+} from './styles'
 
 const ConnectionList: React.FC = () => {
   const [connections] = useRecoilState(connectionsState)
@@ -30,7 +36,44 @@ const ConnectionList: React.FC = () => {
 
           <ul>
             {connections.map(connection => (
-              <li key={connection.name}>{connection.name}</li>
+              <Connection
+                key={connection.name}
+                connected={connection.name === 'Another one'}
+                errored={connection.name === 'Doge'}
+              >
+                <button type="button">
+                  <FiDatabase />
+                  {connection.name}
+                  <FiChevronRight />
+                </button>
+
+                {connection.name === 'Another one' && (
+                  <DatabaseList>
+                    <button type="button">
+                      <strong>db0</strong>
+                      <span>132 keys</span>
+                    </button>
+                    <button type="button">
+                      <strong>db1</strong>
+                      <span>25 keys</span>
+                    </button>
+                    <button type="button">
+                      <strong>db2</strong>
+                      <span>0 keys</span>
+                    </button>
+                    <button type="button">
+                      <strong>db3</strong>
+                      <span>4 keys</span>
+                    </button>
+                  </DatabaseList>
+                )}
+
+                {connection.name === 'Doge' && (
+                  <ConnectionError>
+                    Connection failed. <button type="button">Retry?</button>
+                  </ConnectionError>
+                )}
+              </Connection>
             ))}
           </ul>
         </Connections>
