1ece4f9197549f2a5b9a82c147f99697858bfda0
Diego Fernandes <diego.schell.f@gmail.com>
2020-07-14 22:21:17 -0300

    feat(connection list): list connection databases and store current connection/database
    Currently, the user wasn't able to connect to a database and the current connection and database
    weren't stored in the application state.
    
    Now the user can connect to the saved connections in the
    sidebar and list the databases inside the connection. The user can also connect to a single
    database.
    
    fix #34, fix #35

---
+++ src/services/connection/LoadConnectionDatabases.ts
@@ -0,0 +1,53 @@
+import Redis from 'ioredis'
+
+import { IConnection, IDatabase } from '../../atoms/connections'
+
+export function loadConnectionDatabases(
+  connection: IConnection
+): Promise<IDatabase[]> {
+  return new Promise((resolve, reject) => {
+    const redis = new Redis(connection.port, connection.host, {
+      enableReadyCheck: true,
+      connectTimeout: 3000,
+      password: connection.password,
+      retryStrategy() {
+        return null
+      }
+    })
+
+    redis.once('ready', async () => {
+      const databasesRaw = await redis.info('keyspace')
+
+      const connectionDatabases = databasesRaw
+        .split('\n')
+        .slice(1)
+        .reduce((databases: IDatabase[] = [], databaseLine) => {
+          const matched = /(db[0-9]{1,2}):keys=([0-9]{1,}).+/gi.exec(
+            databaseLine
+          )
+
+          if (matched) {
+            const [, db, keys] = matched
+
+            return [
+              ...databases,
+              {
+                name: db,
+                keys: Number(keys)
+              }
+            ]
+          }
+
+          return databases
+        }, [])
+
+      resolve(connectionDatabases)
+
+      redis.disconnect()
+    })
+
+    redis.once('error', () => {
+      reject(new Error())
+    })
+  })
+}
