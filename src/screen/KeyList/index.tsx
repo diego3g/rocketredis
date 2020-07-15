fdfe07a4f0b02a131b065d437ae338012dd3ffc5
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-07-14 21:56:05 -0300

    chore: create connection details interface
    In this PR I created the connection details interface, with the search input and the list of keys.
    It shows static content only.

--- src/screen/KeyList/index.tsx
+++ src/screen/KeyList/index.tsx
@@ -1,7 +1,20 @@
 import React, { memo } from 'react'
+import { FiFolder } from 'react-icons/fi'
 
 import { useWindowSize } from '../../hooks/useWindowSize'
-import { Container } from './styles'
+import SearchInput from './SearchInput'
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  HeaderTextContainer,
+  HeaderTotalKeys,
+  KeyListContainer,
+  Key,
+  KeyTextContainer,
+  KeyTitle,
+  KeyChidrenCount
+} from './styles'
 
 const KeyList: React.FC = () => {
   const { width } = useWindowSize({ watch: false })
@@ -13,7 +26,48 @@ const KeyList: React.FC = () => {
       minConstraints={[340, Infinity]}
       maxConstraints={[width - 300 - 100, Infinity]}
     >
-      asd
+      <Header>
+        <HeaderTextContainer>
+          <HeaderTitle>Localhost</HeaderTitle>
+          <HeaderTotalKeys>132 keys</HeaderTotalKeys>
+        </HeaderTextContainer>
+        <SearchInput />
+      </Header>
+
+      <KeyListContainer>
+        <Key>
+          <KeyTextContainer>
+            <KeyTitle>mail-queue</KeyTitle>
+            {true && <KeyChidrenCount>(132 keys)</KeyChidrenCount>}
+          </KeyTextContainer>
+
+          {true && <FiFolder />}
+        </Key>
+        <Key>
+          <KeyTextContainer>
+            <KeyTitle>cache</KeyTitle>
+            {true && <KeyChidrenCount>(25 keys)</KeyChidrenCount>}
+          </KeyTextContainer>
+
+          {true && <FiFolder />}
+        </Key>
+        <Key>
+          <KeyTextContainer>
+            <KeyTitle>single-key-without-children</KeyTitle>
+            {false && <KeyChidrenCount>(132 keys)</KeyChidrenCount>}
+          </KeyTextContainer>
+
+          {false && <FiFolder />}
+        </Key>
+        <Key>
+          <KeyTextContainer>
+            <KeyTitle>another-key</KeyTitle>
+            {false && <KeyChidrenCount>(132 keys)</KeyChidrenCount>}
+          </KeyTextContainer>
+
+          {false && <FiFolder />}
+        </Key>
+      </KeyListContainer>
     </Container>
   )
 }
