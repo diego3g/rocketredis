bf223d358bc2d201e3f4c6da6aabcc17012352e4
Muurilo <murilo.henriquecunha551@gmail.com>
2020-07-16 02:00:49 -0300

    fix(fix types): fix types for i18next backend

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,5 @@
 import i18n from 'i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
+import LanguageDetector from 'i18next-electron-language-detector'
 import Backend from 'i18next-fs-backend'
 import { initReactI18next } from 'react-i18next'
 
---
+++ src/global.d.ts
@@ -0,0 +1,2 @@
+declare module 'i18next-fs-backend'
+declare module 'i18next-electron-language-detector'
